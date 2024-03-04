86f5584b5428de10c358d821196fe0d05005dfc6
dipendra <jedeshar@gmail.com>
2024-03-04 11:52:22 +0700

    news -> home page

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:protal_app/Model/newsmodel.dart';
+import 'package:protal_app/components/news_card.dart';
+import 'package:protal_app/viewmodel/newsviewmodel.dart';
 
-void main() {
-  runApp(const HomePage());
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _HomePageState extends State<HomePage> {
+  late NewsController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = NewsController();
+  }
+
+  viewDidLoad() { }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("News App"),
       ),
     );
   }
